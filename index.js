981837aa6d59ab581d01290b41ba56393390109f
My mac <mari_rychkova@yahoo.com>
2018-08-17 01:11:24 +0300

    minor fix

--- index.js
+++ index.js
@@ -48,14 +48,15 @@ require('./controllers/stripeRoutes')(app);
 	Listen requests
 */
 
-if (process.env.process === 'production') {
+if (process.env.NODE_ENV === 'production') {
+
     app.use(express.static('client/build'));
 
     const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+};
 
 const PORT = process.env.PORT || 5000;
 try {
